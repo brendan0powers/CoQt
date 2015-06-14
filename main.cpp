1d532ad2b0f0338a9ad915a9d6d91761582e7fbb
Brendan Powers <bpowers@908devices.com>
2015-06-14 16:37:54 -0400

    Add QFuture and signal examples, add some more comments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QSharedPointer>
+#include <QtConcurrentRun>
+#include <QTimer>
 #include "fiber.h"
 #include "scheduler.h"
 #include "context.h"
+#include "wakecondition.h"
 
 //Simple fiber example showing a yield and manual wake
 void testSimpleFiber()
@@ -35,7 +38,7 @@ void testYield()
        for(int i = 0; i < 5; i++)
        {
            qDebug() << "testYield:" << i;
-           CoQt::Fiber::yield();
+           CoQt::Fiber::yield(); //return after 1 scheduler tick
        }
     });
 }
@@ -45,6 +48,8 @@ void testSleep()
 {
     CoQt::createFiber([&]() {
        qDebug() << "Sleeping for 5 seconds";
+
+       //Yield the fiber for 5 seconds
        CoQt::Fiber::yield(5000);
        qDebug() << "Done";
     });
@@ -57,6 +62,8 @@ void testLambda()
        qDebug() << "will waik when i == 5";
 
        int i = 0;
+       //Run a lambda function on each scheduler
+       //tick, and wake the fiber when it returns true
        CoQt::Fiber::yield([&]() {
            i++;
 
@@ -124,6 +131,78 @@ void testNestedFibers()
     });
 }
 
+//Test the QFuture wake condition
+//uses QtConcurrent::run() to do a task in another thread
+//and pause the fiber while the thread is running. Once
+//the thread returns, the results of the QFuture are available
+void testQFuture()
+{
+    CoQt::createFiber([&]() {
+       qDebug() << "Before Thread";
+
+       //Start a new thread, wait for it to complete
+       QFuture<int> future = CoQt::Fiber::yield(QtConcurrent::run([]() {
+            int i = 0;
+
+            //Currently running in a seperate thread
+            qDebug() << "In Thread";
+
+            for(i = 0; i < 123; i++)
+            {
+                QThread::msleep(25);
+            }
+
+            qDebug() << "Returning";
+
+            return i; //Thread ends here
+       }));
+
+       //Once we get here, the thread is complete, and the QFuture
+       //has the results
+       qDebug() << "Result:" << future.result();
+
+       //Just a test to see if we can exit an application from a fiber
+       QCoreApplication::quit();
+    });
+}
+
+//Start a fiber, yield, and resume when a signal is emitted
+void testSignalWakeStyle1()
+{
+    CoQt::createFiber([&]() {
+       QTimer t;
+       t.setSingleShot(true);
+       t.start(3000);
+
+       qDebug() << "Testing signal wake style 2";
+
+       //Wake the fiber when the specified signal is emitted
+       CoQt::Fiber::yield(&t, SIGNAL(timeout()));
+
+       qDebug() << "Done";
+    });
+}
+
+//Start a fiber, yield, and resume when a signal is emitted
+void testSignalWakeStyle2()
+{
+    CoQt::createFiber([&]() {
+       QTimer t;
+       t.setSingleShot(true);
+       t.start(3000);
+
+       qDebug() << "Testing signal wake style 2";
+
+       CoQt::SignalWakeCondition waker(CoQt::context()->curFiber());
+       QObject::connect(&t, &QTimer::timeout, &waker, &CoQt::SignalWakeCondition::wake);
+
+       //Will yield the fiber indefinately. However, the QTimer will wake it in about 3 seconds
+       CoQt::Fiber::yieldForever();
+
+       qDebug() << "Done";
+    });
+}
+
 int main(int argc, char * argv[])
 {
     QCoreApplication app(argc, argv);
@@ -154,9 +233,10 @@ int main(int argc, char * argv[])
     testLambda();
 */
 
-    testNestedFibers();
+    testSignalWakeStyle2();
 
-    qDebug() << "Hello World!";
-    return app.exec();
+    qDebug() << "Running the event loop";
+    app.exec();
+    qDebug() << "Exiting...";
 }
 
